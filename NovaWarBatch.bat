0c5ca5caef5ff905f6d7c4ddf519533b3e33e08e
unknown <jkim802@zpatliclcssds05.ittl.gtri.org>
2014-12-03 23:44:43 -0500

    nova war result

--- NovaWarBatch.bat
+++ NovaWarBatch.bat
@@ -3,16 +3,16 @@ setlocal enabledelayedexpansion
 
 SET iter=10000
 SET memSize=49
-SET efort="Foreboding Archway"
+SET efort="Illuminary Blockade"
 SEt yfort="Sky Fortress(2)"
 SET find="Optimized"
-SET file="NS_war_skyfort.txt"
+SET file="NS_war_skyfort_IB2.txt"
 
 echo %time%>%file% 
 
 
-for /L %%i in (1,1,%memSize%) do (
-	tuo "NS%%i" "novaGT" gw ordered yf %yfort% ef %efort% -e "Heal all 2" -o=nova\invt%%i.txt -t 6 climb %iter% | find %find% >> %file% 
+for /L %%i in (25,1,%memSize%) do (
+	tuo "NS%%i" "novaGT" gw ordered yf %yfort% ef %efort% -e "Heal all 2" -o=nova\invt%%i.txt -t 8 climb %iter% | find %find% >> %file% 
 )
 
 echo %time%>>%file%
--- WarpWarDefBatch.bat
+++ WarpWarDefBatch.bat
@@ -8,12 +8,12 @@ set efort="Sky Fortress(2)"
 
 
 SET find="Optimized"
-SET file="NS_War_Def.txt"
+SET file="WH_War_Def.txt"
 
 echo %time%>%file% 
 
 for /L %%i in (1,1,%memSize%) do (
-	tuo "NS%%i" "novaGT" gw-defense random yf %yfort% ef %efort% -e "Heal all 2" -o=nova\invt%%i.txt -t 8 climb %iter% | find %find% >> %file% 
+	tuo "WH%%i" "GT200k" gw-defense random yf %yfort% ef %efort% -e "Heal all 2" -o=Warp\invt%%i.txt -t 8 climb %iter% | find %find% >> %file% 
 )
 
 echo %time%>>%file%
