f1e03c6cf31347e34fca7f1a60300b1d136086d0
Joseph Home <joek546@gmail.com>
2014-12-04 19:12:59 -0500

    warp allegiance brawl result

--- WarpBrawlBatch.bat
+++ WarpBrawlBatch.bat
@@ -21,7 +21,7 @@ for /F "tokens=2 delims==" %%a in ('set BGE[') do (
 
 	echo %%a>>%file%
 	for /L %%i in (1,1,%memSize%) do (
-		tuo "WH%%i" "GT200k" gw ordered -e %%a -o=Warp\invt%%i.txt -t 8 climb %iter% | find %find% >> %file% 
+		tuo "WH%%i" "GT200k" gw ordered -e %%a -o=Warp\invt%%i.txt -t 6 climb %iter% | find %find% >> %file% 
 	)
 )
 echo %time%>>%file%
