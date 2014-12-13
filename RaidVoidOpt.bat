6d2a721070724e7190a72b928f83e75b47aa3d08
Joseph Home <joek546@gmail.com>
2014-12-12 19:33:38 -0500

    warp raid sim result

--- RaidVoidOpt.bat
+++ RaidVoidOpt.bat
@@ -22,7 +22,7 @@ for /L %%j in (12,2,20) do (
 for /F "tokens=2 delims==" %%a in ('set fort[') do (
 	echo %%a>>%file%
 	for /L %%i in (1,1,%memSize%) do (
-		tuo "VR%%i" "Carnifex-%%j" raid ordered yf %%a -t 6 -o=Void\invt%%i.txt climb %iter% | find %find% >> %file% 
+		tuo "VR%%i" "Carnifex-%%j" raid ordered yf %%a -t 4 -o=Void\invt%%i.txt climb %iter% | find %find% >> %file% 
 	
 	)
 )
