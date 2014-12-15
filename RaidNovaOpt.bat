424d5f8cc07fbb165b70f7bab06df3e574d79097
Joseph Kim <joseph.kim@gtri.gatech.edu>
2014-12-15 12:34:24 -0500

    upper raid update

--- RaidNovaOpt.bat
+++ RaidNovaOpt.bat
@@ -1,7 +1,7 @@
 
 setlocal enabledelayedexpansion
 
-SET iter=60000
+SET iter=100000
 SET memSize=50
 
 
@@ -12,11 +12,11 @@ set fort[02]="Lightning Cannon,Inspiring Altar"
 set fort[03]="Inspiring Altar(2)"
 
 SET find="Optimized"
-SET file="Carnifex_Nova_opt.txt"
+SET file="Carnifex_Nova_opt2.txt"
 
 echo %time%>%file% 
 
-for /L %%j in (12,2,20) do (
+for /L %%j in (17,2,23) do (
 	echo.>>%file%
 	echo level %%j>>%file%
 for /F "tokens=2 delims==" %%a in ('set fort[') do (
