f8d64be5efc612b695d07c1d87c66a521e05d1a5
Joseph Kim Z800 <joseph.kim@gtri.gatech.edu>
2015-02-18 20:19:39 -0500

    nextcard, invent update

--- nextCardbatch.bat
+++ nextCardbatch.bat
@@ -13,14 +13,14 @@ echo %time%>>%file%
 echo.>>%file%
 
 echo Current Best Deck:>>%file%
-tuo %mydeck% "Bairs" gw ordered -o=invent.txt -t 6 climb %iter% | find %find% >> %file% 
+tuo %mydeck% "GTD" gw ordered -o=invent.txt -t 6 climb %iter% | find %find% >> %file% 
 echo.>>%file%
 
 for /F "tokens=*" %%i in (nextcard.txt) do (
 	type invent.txt > thisdeck.txt
 	echo %%i>>thisdeck.txt
 	echo with %%i:>>%file%
-	tuo %mydeck% "Bairs" gw ordered -o=thisdeck.txt -t 6 climb %iter% | find %find% >> %file% 
+	tuo %mydeck% "GTD" gw ordered -o=thisdeck.txt -t 6 climb %iter% | find %find% >> %file% 
 	echo.>>%file%
 )
 
