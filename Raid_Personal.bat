4e0499250c3c08bb7ad7cc157e03d7a4b0d52694
Joseph Kim Z800 <joseph.kim@gtri.gatech.edu>
2015-02-20 17:10:39 -0500

    Brood Mother raid result

--- Raid_Personal.bat
+++ Raid_Personal.bat
@@ -20,7 +20,7 @@ for /F "tokens=2 delims==" %%a in ('set fort[') do (
 	echo %%a>>%file%
 	for /L %%j in (15,1,22) do (
 		echo level %%j>>%file%
-		tuo nkorboy "Brood Mother-%%j" raid ordered yf %%a -t 6 -o=invent.txt climb %iter% | find %find% >> %file% 
+		tuo nkorboy "Brood Mother-%%j" raid ordered yf %%a -t 8 -o=invent.txt climb %iter% | find %find% >> %file% 
 	)
 )
 echo %time%>>%file%
